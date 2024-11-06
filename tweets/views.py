0d3dd4180402ef4c448c7898d8df91880b292d77
“wlsto” <junhu84@gmail.com>
2024-11-06 21:04:30 +0900

    assignment_ViewTweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet
+
 
 # Create your views here.
+def getAllTweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "all_tweets.html",
+        {
+            "tweets": tweets,
+            "title": "Hello~ Welcome to our tweets!!",
+        },
+    )
